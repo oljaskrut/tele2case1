35651d343265519116dd9f00eaaa2658af5771b9
Olzhas <oljaskrutf@gmail.com>
2021-06-20 21:27:33 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+//импорт библиотек
 const express = require('express')
 const mysql = require('mysql')
 
-
+//подключение к базе данных
 const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -10,8 +11,10 @@ const db = mysql.createConnection({
 })
 
 db.connect(function (e){
+  //проверка на ошибки
   if(e) throw e
   
+  //создание таблицы если таковой нет
   db.query('
     CREATE TABLE IF NOT EXISTS `users` (
     `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
@@ -25,45 +28,44 @@ db.connect(function (e){
    ', (e, _) => if (e) throw e)
 })
 
-/*
-
-con.query(`INSERT INTO customers (name, address) VALUES (${}, ${})`, (e, _) => if (e) throw e)
-
-*/
-
-
-
+//инциализация API
 const app = express()
+//подключение парсера для входных данных
 app.use(express.bodyParser())
-
+//настройка GET запроса (READ)
 app.get('/users/:id', (req, res) => {
+  //переменная id из параметра запроса
   const id = req.params.id
-  
+  //в случае отсутствия id возвращает все записи
   if(!id){
     db.query(`SELECT * FROM users`, (e, r,_) => {
       res.send(r)
     })
   }else{
+    //возвращает запись с определенным id
     db.query(`SELECT * FROM users WHERE id=${id}`, (e, r,_) => {
       res.send(r)
     })
   }
 })
-
+//настройка POST запроса (CREATE)
 app.post('/users', (req, res) => {
+  //деструкризация тела запроса
   const {name, login, password, birthday, info, phone} = req.body
   db.query(`INSERT INTO users (name, login, password, birthday, info, phone) VALUES (${name}, ${login}, ${password}, ${birthday}, ${info}, ${phone})`, (e, _) => if (e) throw e) 
 })
-
+//настройка PUT запроса (UPDATE)
 app.put('/users/:id', (req, res) => {
   const id = req.params.id
+  {
+  //деструкризация тела запроса
   const {name, login, password, birthday, info, phone} = req.body
   db.query(`INSERT INTO users (name, login, password, birthday, info, phone) VALUES (${name}, ${login}, ${password}, ${birthday}, ${info}, ${phone}) WHERE id=${id}`, (e, _) => if (e) throw e)
  })
-
+//настройка DELETE запроса (DELETE)
 app.delete('/users/:id', (req, res) => {
   const id = req.params.id
   db.query(`DELETE FROM users WHERE id=${id}`, (e, _) => if (e) throw e)
 })
-
+//запускает сервер на порту 8000
 app.listen(8000)
