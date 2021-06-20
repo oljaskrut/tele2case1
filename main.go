b0c9ec401b1a5cadb866883c4f349477eb2b425b
Olzhas <oljaskrutf@gmail.com>
2021-06-20 17:22:07 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+/*
+SQL query
+
+CREATE TABLE `user` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `birthday` Date NOT NULL,
+  `info` varchar(150) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+*/
+
+#main code
+
